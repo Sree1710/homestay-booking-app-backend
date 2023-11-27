b2dd5fcaf15c0aa8d7e41c1071b24a7f82a2236a
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-27 14:37:38 +0530

    Modifications Done

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post("/userlog",async(request,response)=>{
         if (result[0].password==getPassword) {
             response.json({"status":"success","data":result[0]})
         } else {
-            response.json({"status":"Inavlid Username or Password !!!"})
+            response.json({"status":"Invalid Username or Password !!!"})
         }
     } else {
         response.json({"status":"User Does Not Exist !!!"})
