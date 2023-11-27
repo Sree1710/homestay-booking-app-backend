977100155aa6e9751cb54d52ef8a069a6af2441e
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-27 14:17:52 +0530

    UserReg backend completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Express=require('express')
 const Cors=require('cors')
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
+const userModel = require('./userModel')
 
 const app=Express()
 app.use(bodyParser.json())
@@ -9,6 +10,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(Cors())
 
 mongoose.connect("mongodb+srv://sreelekshmisl1710:Dharithri@cluster0.y83cozw.mongodb.net/homestayDB?retryWrites=true&w=majority")
+
+app.post("/userreg",async(request,response)=>{
+    let data=request.body
+    const reg=new userModel(data)
+    let result=await reg.save()
+    if (result.Name!="") {
+        response.json({"status":"success"})
+    } else {
+        response.json({"status":"error"})
+    }
+})
 
 
 
